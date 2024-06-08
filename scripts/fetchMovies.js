b5306e4d7431d756253e8a3e4625118b106cd800
rene guardia <reneguardia@outlook.com>
2024-06-08 16:45:49 -0300

    Refactor

--- scripts/fetchMovies.js
+++ scripts/fetchMovies.js
@@ -1,6 +1,7 @@
 const API_KEY = '258a10d91fc0ae505bc7bbf0d3b31c0e';
 const LANGUAGE = 'en-US';
 const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500/';
 
 const popularMoviesContainer = document.getElementById("trending-today");
 const acclaimedMoviesContainer = document.querySelector('.acclaimed-movies');
@@ -11,31 +12,39 @@ let popularMoviesPage = 1;
 
 const buildURL = (type, page) => `${BASE_URL}${type}?api_key=${API_KEY}&language=${LANGUAGE}&page=${page}`;
 
-const loadMovies = async (type, container, page) => {
+const fetchMovies = async (url) => {
     try {
-        const response = await fetch(buildURL(type, page));
-        if (!response.ok) {
-            throw new Error('Hubo un error al cargar las películas.');
-        }
-        const data = await response.json();
-        displayMovies(data.results, container);
+        const response = await fetch(url);
+        if (!response.ok) throw new Error('Failed to load movies');
+        return await response.json();
     } catch (error) {
-        console.error('Error al cargar películas:', error.message);
+        console.error('Error fetching movies:', error);
+        throw error;
     }
 }
 
-const displayMovies = (movies, container) => {
-    container.innerHTML = '';
-    movies.forEach(movie => {
-        const movieHTML = `
-        <a href="./pages/detail.html">
-            <div class="movie">
-                    <img class="trending-image" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}">
-                    <div class="movie-title"><h4>${movie.title}</h4></div>
+const loadMovies = async (type, container, page) => {
+    try {
+        const url = buildURL(type, page);
+        const data = await fetchMovies(url);
+        displayMovies(data.results, container);
+    } catch (error) {
+        container.innerHTML = '<p>Failed to load movies. Please try again later.</p>';
+    }
+}
+
+const createMovieHTML = (movie) =>
+    `<a href="./pages/detail.html">
+        <div class="movie">
+            <img class="trending-image" src="${IMAGE_BASE_URL}${movie.poster_path}" alt="${movie.title}">
+            <div class="movie-title">
+                <h4>${movie.title}</h4>
             </div>
-        </a>`;
-        container.insertAdjacentHTML('beforeend', movieHTML);
-    });
+        </div>
+     </a>`;
+
+const displayMovies = (movies, container) => {
+    container.innerHTML = movies.map(createMovieHTML).join('');
 }
 
 const updatePage = (type, value) => {
